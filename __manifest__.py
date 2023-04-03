428f3cacb5e0fd6ecda9352e25eaf4bb2389b932
MykolaSubota <mykola.subota@uzhnu.edu.ua>
2023-04-03 14:38:11 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Privat bank',
+    'author': 'Mykola Subota',
+    'version': '0.1',
+    'summary': 'Privat bank',
+    'sequence': 100,
+    'category': 'Inventory/Inventory',
+    'depends': ['base', 'account'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
+
